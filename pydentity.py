9e597b510d5f4a50a98405813eee0bd6367f063e
Sebastien Renard <Sebastien.Renard@digitalfox.org>
2015-08-20 00:46:35 +0200

    Very very basic, but working, web application to manage apache htpasswd files

---
+++ pydentity.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+"""
+Mini application to manage apache htpasswd file
+@author: Sébastien Renard (sebastien.renard@digitalfox.org)
+@license: AGPL v3 or newer (http://www.gnu.org/licenses/agpl-3.0.html)
+"""
+
+import subprocess
+
+from flask import Flask, render_template, request
+import htpasswd
+
+
+app = Flask(__name__)
+
+
+PWD_FILE = "htpasswd"
+
+
+@app.route("/")
+def home():
+    if request.headers.get("REMOTE_USER"):
+        return user(request.headers.get("REMOTE_USER"))
+    return "Hello World!"
+
+
+@app.route('/user/list')
+def list_users():
+    with htpasswd.Basic(PWD_FILE, mode="md5") as userdb:
+        return render_template("list.html", users=userdb.users)
+
+
+@app.route("/user/<username>", methods=['POST', 'GET'])
+def user(username):
+    with htpasswd.Basic(PWD_FILE, mode="md5") as userdb:
+        if request.method == "GET":
+            if username in userdb:
+                return render_template("user.html", username=username)
+            else:
+                return "Unknown user %s" % username
+        else:
+            if request.form["new_password"] != request.form["repeat_password"]:
+                return "Password differ. Please hit back and try again"
+            if not check_password(userdb.new_users[username], request.form["old_password"]):
+                return "password does not match"
+            else:
+                # Ok, ready to change password
+                userdb.change_password(username, request.form["new_password"])
+                return "Password changed"
+
+
+def check_password(encrypted_passwd, clear_passwd, mode="md5"):
+    """check that password is correct against its hash"""
+    salt = encrypted_passwd.split("$")[2]  # Extract salt from current encrypted password
+    new_encrypted_passwd = subprocess.check_output(["openssl", "passwd", "-apr1", "-salt", salt, clear_passwd]).decode('utf-8')
+    return encrypted_passwd == new_encrypted_passwd
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
