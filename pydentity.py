5acdc856df26907efc98ab8134e95b675f2db77c
Sebastien Renard <Sebastien.Renard@digitalfox.org>
2015-10-14 23:37:50 +0200

    Fix help message

--- pydentity.py
+++ pydentity.py
@@ -26,7 +26,7 @@ CONF = {
     # New password pattern regexp check. Note that this regexp must be compliant to both Python regexp syntax and HTML 5 form pattern syntax
     "PASSWORD_PATTERN": "(?=.*\d)(?=.*[a-z])(?=.*[A-Z\!\@\#\$\%\^\&\*\-]).{8,}",
     # Clear text that explain to user the password requirements
-    "PASSWORD_PATTERN_HELP" : "Upper and lower case, numeric. At least 8 char",
+    "PASSWORD_PATTERN_HELP" : "Lower case, numeric and upper case or special char. At least 8 char",
 }
 
 
