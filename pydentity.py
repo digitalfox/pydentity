53a6de99442b60fc9c6cd1870a9ed1cea42c3ffe
Fabien Béziaud <fabien.beziaud@enioka.com>
2019-03-23 11:52:07 +0100

    First naive implementation of batch user creation
    * With randow password generator
    * With batch user creation screen
    * Showing the result in message template screen
    * Fixing use of HEADER in request

--- pydentity.py
+++ pydentity.py
@@ -11,6 +11,7 @@ from re import match
 
 from flask import Flask, render_template, request, redirect, url_for
 import htpasswd
+import random
 
 
 app = Flask(__name__)
@@ -24,16 +25,18 @@ CONF = {
     # Whether to require http basic auth upstream (for example with apache)
     "REQUIRE_REMOTE_USER": True,
     # New password pattern regexp check. Note that this regexp must be compliant to both Python regexp syntax and HTML 5 form pattern syntax
-    "PASSWORD_PATTERN": "(?=.*\d)(?=.*[a-z])(?=.*[A-Z\!\@\#\$\%\^\&\*\-]).{8,}",
+    "PASSWORD_PATTERN": "(?=.*\d)(?=.*[a-z])(?=.*[A-Z\!\@\#\$\%\^\&\*\-\+\;\?\.\!\_\=\(\)\[\]\{\}]).{8,}",
     # Clear text that explain to user the password requirements
     "PASSWORD_PATTERN_HELP" : "Upper and lower case, numeric. At least 8 char",
+    # List of used chars for password generation
+    "PASSWORD_GENERATION_CHARS": "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*-+;?.!_=()[]{}"
 }
 
 
 @app.route("/")
 def home():
-    if request.environ.get('REMOTE_USER'):
-        url = url_for("user", username=request.environ.get('REMOTE_USER'))
+    if "REMOTE_USER" in request.headers:
+        url = url_for("user", username=request.headers["REMOTE_USER"])
         if "return_to" in request.args:
             url += "?return_to=%s" % request.args.get("return_to")
     else:
@@ -50,14 +53,20 @@ def list_users():
 def user(username):
     with htpasswd.Basic(CONF["PWD_FILE"], mode="md5") as userdb:
         new_user = username not in userdb
-        admin, admin_error_message = check_user_is_admin(request.environ.get('REMOTE_USER'))
-        ask_old_password = True
-        if admin and request.environ.get('REMOTE_USER') != username:
-            ask_old_password = False
+        if "REMOTE_USER" in request.headers:
+            admin, admin_error_message = check_user_is_admin(request.headers["REMOTE_USER"])
+        else:
+            # No REMOTE_USER header, can't work
+            message = "Can't work without REMOTE_USER header, contact an administrator"
+            return render_template("message.html", success=False, message=message)
+        admin_feature = False
+        if admin and request.headers["REMOTE_USER"] != username:
+            admin_feature = True
+
         if CONF["REQUIRE_REMOTE_USER"]:
-            if not request.environ.get('REMOTE_USER'):
+            if not request.headers["REMOTE_USER"]:
                 return render_template("message.html", message="Sorry, you must be logged with http basic auth to go here")
-            if request.environ.get('REMOTE_USER') != username or new_user:
+            if request.headers["REMOTE_USER"] != username or new_user:
                 # User trying to change someone else password
 
                 if not admin:
@@ -65,13 +74,32 @@ def user(username):
                     return render_template("message.html", message=admin_error_message)
 
         if request.method == "GET":
-            return render_template("user.html", username=username, new=new_user, ask_old_password=ask_old_password,
+            return render_template("user.html", username=username, new=new_user, admin_feature=admin_feature,
                                    password_pattern=CONF["PASSWORD_PATTERN"])
         else:
             # POST Request
+            # If the generate random password is pressed
+            if 'generaterandom' in request.form:
+                new_password = generate_random_password()
+                i = 0
+                while not match(CONF["PASSWORD_PATTERN"], new_password):
+                    new_password = generate_random_password()
+                    i += 1
+                    if i > 10:
+                        # To prevent infinite loop
+                        return render_template("message.html", message="Something wrong with password generation. Please try again.")
+                if new_user:
+                    userdb.add(username, new_password)
+                    message = "User created with random password: %s" % new_password
+                    return render_template("message.html", message=message, success=True)
+                else:
+                    userdb.change_password(username, new_password)
+                    message = "User password changed with random password: %s" % new_password
+                    return render_template("message.html", message=message, success=True)
+            # If the validate button is pressed
             if request.form["new_password"] != request.form["repeat_password"]:
                 return render_template("message.html", message="Password differ. Please hit back and try again")
-            if ask_old_password and not check_password(userdb.new_users[username], request.form["old_password"]):
+            if not admin_feature and not check_password(userdb.new_users[username], request.form["old_password"]):
                 return render_template("message.html", message="password does not match")
             if not match(CONF["PASSWORD_PATTERN"], request.form["new_password"]):
                 return render_template("message.html", message="new password does not match requirements (%s" % CONF["PASSWORD_PATTERN_HELP"])
@@ -90,11 +118,15 @@ def user(username):
 
 @app.route("/user_groups/<username>", methods=["POST", "GET"])
 def user_groups(username):
-
-    admin, message = check_user_is_admin(request.environ.get('REMOTE_USER'))
-    if not admin:
-        # User is not admin or admin group does exist. Ciao
-        return render_template("message.html", message=message)
+    if "REMOTE_USER" in request.headers:
+        admin, message = check_user_is_admin(request.headers["REMOTE_USER"])
+        if not admin:
+            # User is not admin or admin group does exist. Ciao
+            return render_template("message.html", message=message)
+    else:
+        # No REMOTE_USER header, can't work
+        message="Can't work without REMOTE_USER header, contact an administrator"
+        return render_template("message.html", success=False, message=message)
 
     with htpasswd.Basic(CONF["PWD_FILE"], mode="md5") as userdb:
         with htpasswd.Group(CONF["GROUP_FILE"]) as groupdb:
@@ -108,20 +140,68 @@ def user_groups(username):
                 return render_template("groups.html", groups=groups)
             else:
                 # POST Request
-                print request.form.items()
                 checked_groups = [g.split("_", 1)[1] for g in request.form.keys() if g.startswith("group_")]
-                print checked_groups
                 for group in groupdb.groups:
                     if group in checked_groups:
                         if not groupdb.is_user_in(username, group):
-                            print "add user to group %s" % group
                             groupdb.add_user(username, group)
                     else:
                         if groupdb.is_user_in(username, group):
-                            print "remove user from group %s" % group
                             groupdb.delete_user(username, group)
                 return render_template("message.html", message="User groups changed", success=True)
 
+
+@app.route("/batch_user_creation", methods=["POST", "GET"])
+def batch_user_creation():
+    if "REMOTE_USER" in request.headers:
+        admin, message = check_user_is_admin(request.headers["REMOTE_USER"])
+        if not admin:
+            # User is not admin or admin group does exist. Ciao
+            return render_template("message.html", message=message)
+    else:
+        # No REMOTE_USER header, can't work
+        message="Can't work without REMOTE_USER header, contact an administrator"
+        return render_template("message.html", success=False, message=message)
+
+    with htpasswd.Basic(CONF["PWD_FILE"], mode="md5") as userdb:
+        with htpasswd.Group(CONF["GROUP_FILE"]) as groupdb:
+            if request.method == "GET":
+                groups = []
+                for group in groupdb.groups:
+                    groups.append(group)
+                return render_template("batch_user_creation.html", groups=groups)
+            else:
+                # POST Request
+                users = request.form["users_login"].split("\r\n")
+                checked_groups = [g.split("_", 1)[1] for g in request.form.keys() if g.startswith("group_")]
+                result = []
+                for username in users:
+                    new_password = generate_random_password()
+                    i = 0
+                    while not match(CONF["PASSWORD_PATTERN"], new_password):
+                        new_password = generate_random_password()
+                        i += 1
+                        if i > 10:
+                            # To prevent infinite loop
+                            return render_template("message.html",
+                                                   message="Something wrong with password generation. Please try again.")
+                    new_user = username not in userdb
+                    if new_user:
+                        userdb.add(username, new_password)
+                        action = "create"
+                    else:
+                        userdb.change_password(username, new_password)
+                        action = "update"
+                    result.append((username, new_password, action))
+                    for group in groupdb.groups:
+                        if group in checked_groups:
+                            if not groupdb.is_user_in(username, group):
+                                groupdb.add_user(username, group)
+                        else:
+                            if groupdb.is_user_in(username, group):
+                                groupdb.delete_user(username, group)
+                message = "Batch of user created with generated passwords"
+                return render_template("message.html", message=message, success=True, result=result)
 
 
 def check_user_is_admin(user):
@@ -142,6 +222,12 @@ def check_password(encrypted_passwd, clear_passwd, mode="md5"):
     salt = encrypted_passwd.split("$")[2]  # Extract salt from current encrypted password
     new_encrypted_passwd = subprocess.check_output(["openssl", "passwd", "-apr1", "-salt", salt, clear_passwd]).decode('utf-8')
     return encrypted_passwd == new_encrypted_passwd
+
+
+def generate_random_password(length=8):
+    """Generate a random password of the desired length
+    @return a generated password of the desired length"""
+    return "".join(random.sample(CONF["PASSWORD_GENERATION_CHARS"], length))
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -52,56 +52,56 @@ class BasicTestCase(unittest.TestCase):
 
 
     def test_redirect_auth_user_to_its_page(self):
-        r = self.client.get("/", environ_base = { "REMOTE_USER": "user42" })
+        r = self.client.get("/", headers={"REMOTE_USER": "user42"})
         self.assertEqual(r.status_code, 302)
         self.assertIn("/user/user42", r.location)
-        r = self.client.get("/?return_to=/lala", environ_base = { "REMOTE_USER": "user42" })
+        r = self.client.get("/?return_to=/lala", headers={"REMOTE_USER": "user42"})
         self.assertEqual(r.status_code, 302)
         self.assertIn("/user/user42?return_to=/lala", r.location)
 
 
     def test_redirect_after_pwd_change(self):
-        r = self.client.post("/user/user2?return_to=/lala", data = {"old_password": "user2", "new_password": "New12345", "repeat_password":"New12345"}, environ_base = { "REMOTE_USER": "user2" })
+        r = self.client.post("/user/user2?return_to=/lala", data = {"old_password": "user2", "new_password": "New12345", "repeat_password":"New12345"}, headers={"REMOTE_USER": "user2"})
         self.assertEqual(r.status_code, 302)
         self.assertIn("/lala", r.location)
 
 
     def test_change_my_passwd(self):
         for user in ("user1", "user2"):
-            r = self.client.get("/user/%s" % user, environ_base = { "REMOTE_USER": "%s" % user })
+            r = self.client.get("/user/%s" % user, headers={"REMOTE_USER": "%s" % user })
             self.assertEqual(r.status_code, 200)
             self.assertIn("Change password for user %s" % user, r.data)
             self.assertIn("old_password", r.data)
-            r = self.client.post("/user/%s" % user, data = {"old_password": "%s" % user, "new_password": "New12345", "repeat_password":"New12345"}, environ_base = { "REMOTE_USER": "%s" % user })
+            r = self.client.post("/user/%s" % user, data = {"old_password": "%s" % user, "new_password": "New12345", "repeat_password":"New12345"}, headers={"REMOTE_USER": "%s" % user })
             self.assertEqual(r.status_code, 200)
             self.assertIn("Password changed", r.data)
-            r = self.client.post("/user/%s" % user, data = {"old_password": "New12345", "new_password": "New12345678", "repeat_password":"New12345678"}, environ_base = { "REMOTE_USER": "%s" % user })
+            r = self.client.post("/user/%s" % user, data = {"old_password": "New12345", "new_password": "New12345678", "repeat_password":"New12345678"}, headers={"REMOTE_USER": "%s" % user })
             self.assertEqual(r.status_code, 200)
             self.assertIn("Password changed", r.data)
-            r = self.client.post("/user/%s" % user, data = {"old_password": "New12345678", "new_password": "new123456!", "repeat_password":"new123456!"}, environ_base = { "REMOTE_USER": "%s" % user })
+            r = self.client.post("/user/%s" % user, data = {"old_password": "New12345678", "new_password": "new123456!", "repeat_password":"new123456!"}, headers={"REMOTE_USER": "%s" % user })
             self.assertEqual(r.status_code, 200)
             self.assertIn("Password changed", r.data)
-            r = self.client.post("/user/%s" % user, data = {"old_password": "new123456!", "new_password": "New!123456", "repeat_password":"New!123456"}, environ_base = { "REMOTE_USER": "%s" % user })
+            r = self.client.post("/user/%s" % user, data = {"old_password": "new123456!", "new_password": "New!123456", "repeat_password":"New!123456"}, headers={"REMOTE_USER": "%s" % user })
             self.assertEqual(r.status_code, 200)
             self.assertIn("Password changed", r.data)
-            r = self.client.post("/user/%s" % user, data = {"old_password": "New!123456", "new_password": "new$!^-99", "repeat_password":"new$!^-99"}, environ_base = { "REMOTE_USER": "%s" % user })
+            r = self.client.post("/user/%s" % user, data = {"old_password": "New!123456", "new_password": "new$!^-99", "repeat_password":"new$!^-99"}, headers={"REMOTE_USER": "%s" % user })
             self.assertEqual(r.status_code, 200)
             self.assertIn("Password changed", r.data)
-            r = self.client.post("/user/%s" % user, data = {"old_password": "new$!^-99", "new_password": "$#!^9NEw@&*-", "repeat_password":"$#!^9NEw@&*-"}, environ_base = { "REMOTE_USER": "%s" % user })
+            r = self.client.post("/user/%s" % user, data = {"old_password": "new$!^-99", "new_password": "$#!^9NEw@&*-", "repeat_password":"$#!^9NEw@&*-"}, headers={"REMOTE_USER": "%s" % user })
             self.assertEqual(r.status_code, 200)
             self.assertIn("Password changed", r.data)
 
 
 
     def test_new_user(self):
-        r = self.client.get("/user/xxx", environ_base = { "REMOTE_USER": "xxx" })
+        r = self.client.get("/user/xxx", headers={"REMOTE_USER": "xxx" })
         self.assertEqual(r.status_code, 200)
         self.assertNotIn("Creation of user xxx", r.data)
-        r = self.client.get("/user/xxx", environ_base = { "REMOTE_USER": "user1" })
+        r = self.client.get("/user/xxx", headers={"REMOTE_USER": "user1"})
         self.assertEqual(r.status_code, 200)
         self.assertNotIn("old_password", r.data)
         self.assertIn("Creation of user xxx", r.data)
-        r = self.client.post("/user/xxx", data = {"new_password": "New12345", "repeat_password":"New12345"}, environ_base = { "REMOTE_USER": "user1" })
+        r = self.client.post("/user/xxx", data = {"new_password": "New12345", "repeat_password":"New12345"}, headers={"REMOTE_USER": "user1"})
         self.assertEqual(r.status_code, 200)
         self.assertIn("User created", r.data)
         with htpasswd.Basic(self.passwd, mode="md5") as userdb:
@@ -109,20 +109,20 @@ class BasicTestCase(unittest.TestCase):
 
 
     def test_change_someone_else_pwd_as_admin(self):
-        r = self.client.get("/user/user2", environ_base = { "REMOTE_USER": "user1" })
+        r = self.client.get("/user/user2", headers={"REMOTE_USER": "user1"})
         self.assertEqual(r.status_code, 200)
         self.assertIn("Change password for user user2", r.data)
         self.assertNotIn("Old password", r.data)
-        r = self.client.post("/user/user2", data = {"new_password": "New12345", "repeat_password":"New12345"}, environ_base = { "REMOTE_USER": "user1" })
+        r = self.client.post("/user/user2", data = {"new_password": "New12345", "repeat_password":"New12345"}, headers={"REMOTE_USER": "user1"})
         self.assertEqual(r.status_code, 200)
         self.assertIn("Password changed", r.data)
 
 
     def test_change_someone_else_pwd_as_nobody(self):
-        r = self.client.get("/user/user1", environ_base = { "REMOTE_USER": "user2" })
+        r = self.client.get("/user/user1", headers={"REMOTE_USER": "user2"})
         self.assertEqual(r.status_code, 200)
         self.assertIn("Sorry, you must belongs to group", r.data)
-        r = self.client.post("/user/user1", data = {"new_password": "New12345", "repeat_password":"New12345"}, environ_base = { "REMOTE_USER": "user2" })
+        r = self.client.post("/user/user1", data = {"new_password": "New12345", "repeat_password":"New12345"}, headers={"REMOTE_USER": "user2"})
         self.assertEqual(r.status_code, 200)
         self.assertIn("Sorry, you must belongs to group", r.data)
 
@@ -132,14 +132,14 @@ class BasicTestCase(unittest.TestCase):
             ({"old_password": "XXXXX", "new_password": "New12345", "repeat_password":"New12345"}, "password does not match"),
             ({"old_password": "user2", "new_password": "New12345", "repeat_password":"New12345678"}, "Password differ"),
             ({"old_password": "user2", "new_password": "new", "repeat_password":"new"},"password does not match requirement")]:
-            r = self.client.post("/user/user2", data = data, environ_base = { "REMOTE_USER": "user2" })
+            r = self.client.post("/user/user2", data = data, headers={"REMOTE_USER": "user2"})
             self.assertEqual(r.status_code, 200)
             self.assertNotIn("Password changed", r.data)
             self.assertIn(error_message, r.data)
 
 
     def test_add_group(self):
-        r = self.client.get("/user_groups/user1", environ_base = { "REMOTE_USER": "user1" })
+        r = self.client.get("/user_groups/user1", headers={"REMOTE_USER": "user1"})
         self.assertEqual(r.status_code, 200)
         for group in ("users", "admin"):
             self.assertIn('''name="group_%s" type="checkbox" checked''' % group, r.data)
@@ -147,7 +147,7 @@ class BasicTestCase(unittest.TestCase):
         with htpasswd.Group(self.group) as groupdb:
             self.assertTrue(groupdb.is_user_in("user1", "users"))
 
-        r = self.client.post("/user_groups/user1", data = {"group_admin": "on"}, environ_base = { "REMOTE_USER": "user1"})
+        r = self.client.post("/user_groups/user1", data = {"group_admin": "on"}, headers={"REMOTE_USER": "user1"})
 
         with htpasswd.Group(self.group) as groupdb:
             self.assertEqual(r.status_code, 200)
@@ -156,11 +156,11 @@ class BasicTestCase(unittest.TestCase):
 
 
     def test_change_group_without_admin(self):
-        r = self.client.get("/user_groups/user2", environ_base = { "REMOTE_USER": "user2" })
+        r = self.client.get("/user_groups/user2", headers={"REMOTE_USER": "user2"})
         self.assertEqual(r.status_code, 200)
         self.assertIn("Sorry, you must belongs to group", r.data)
 
-        r = self.client.post("/user_groups/user2", data = {"group_admin": "on"}, environ_base = { "REMOTE_USER": "user2"})
+        r = self.client.post("/user_groups/user2", data = {"group_admin": "on"}, headers={"REMOTE_USER": "user2"})
         self.assertEqual(r.status_code, 200)
         self.assertIn("Sorry, you must belongs to group", r.data)
 
